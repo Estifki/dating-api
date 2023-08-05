010031650b0399556311716f66dd30ad6d9a04f1
Estif <estif0919@gmail.com>
2023-08-05 09:05:54 +0300

    user schema bug fixed

--- src/user/schema/user.shema.ts
+++ src/user/schema/user.shema.ts
@@ -1,43 +1,28 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IsArray, IsDate, IsEmpty, IsString } from 'class-validator';
-import { type } from 'os';
 
 @Schema({
   timestamps: true,
 })
 export class User {
   @Prop({ isRequired: true, unique: true })
-  @IsEmpty()
-  @IsString()
   username: string;
 
   @Prop({ isRequired: true })
-  @IsEmpty()
-  @IsString()
   firstname: string;
 
   @Prop({ isRequired: true })
-  @IsEmpty()
-  @IsString()
   lastname: string;
 
   @Prop({ isRequired: true })
-  @IsEmpty()
-  @IsDate()
   dob: Date;
 
   @Prop({ isRequired: true })
-  @IsEmpty()
-  @IsString()
   bio: string;
 
   @Prop({ required: true, type: [String] })
-  @IsEmpty()
-  @IsArray()
   hobbies: string[];
 
   @Prop()
-  @IsString()
   profilepic: string;
 }
 
