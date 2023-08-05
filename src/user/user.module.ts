32699922e86f611d0361ae4cf23b39aa4a6c4f3f
Estif <estif0919@gmail.com>
2023-08-05 09:03:58 +0300

    added user schema

---
+++ src/user/user.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { userSchema } from './schema/user.shema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: userSchema,
+      },
+    ]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
