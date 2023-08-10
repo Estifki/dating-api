5fb11c96d59a5aa83f8e04f0b484acb34fe76170
Estifki <estif0919@gmail.com>
2023-08-10 16:49:05 +0300

    find all user added

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schema/user.shema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel('User') private userModel: Model<User>) {}
+
+  async getAllUsers() {
+    return await this.userModel.find();
+  }
+}
